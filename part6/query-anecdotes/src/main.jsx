3bbc16fee5d9459178460c574dff569b9abe9d82
Alexi <alexivion29@gmail.com>
2026-08-23 20:59:27 -0300

    6.20: query-anecdotes step1 - fetch anecdotes using React Query

---
+++ part6/query-anecdotes/src/main.jsx
@@ -0,0 +1,7 @@
+﻿import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdo<QueryClientProvider client={queryClient}>
+    <NotificationContextProvider>
+      <App />
+    </NotificationContextProvider>
+  </QueryClientProvider>
+)
