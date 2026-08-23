3aae37110243a4aafaa3ce67b76865b3a9603ab3
Alexi <alexivion29@gmail.com>
2026-08-23 20:48:02 -0300

    part5: exercise 5.21 bloglist e2e step5

--- part5/bloglist-e2e/tests/blog_app.spec.js
+++ part5/bloglist-e2e/tests/blog_app.spec.js
@@ -5,10 +5,6 @@ describe('Blog app', () => {
     await page.goto('http://localhost:5173')
   })
 
-  test('Login form is shown', async ({ page }) => {
-    await expect(page.getByText('Log in to application')).toBeVisible()
-  })
-
   describe('When logged in', () => {
     beforeEach(async ({ page }) => {
       await page.locator('input[name="Username"]').fill('root')
@@ -16,28 +12,20 @@ describe('Blog app', () => {
       await page.getByRole('button', { name: 'login' }).click()
     })
 
-    test('a new blog can be created', async ({ page }) => {
+    test('user who created a blog can delete it', async ({ page }) => {
       await page.getByRole('button', { name: 'new blog' }).click()
-      await page.locator('form input').nth(0).fill('E2E Test Title')
-      await page.locator('form input').nth(1).fill('E2E Test Author')
-      await page.locator('form input').nth(2).fill('http://e2etest.com')
+      await page.locator('form input').nth(0).fill('Blog to Delete')
+      await page.locator('form input').nth(1).fill('Delete Author')
+      await page.locator('form input').nth(2).fill('http://delete.com')
       await page.getByRole('button', { name: 'create' }).click()
 
-      await expect(page.getByText('E2E Test Title E2E Test Author')).toBeVisible()
-    })
-
-    test('a blog can be liked', async ({ page }) => {
-      await page.getByRole('button', { name: 'new blog' }).click()
-      await page.locator('form input').nth(0).fill('Blog to Like')
-      await page.locator('form input').nth(1).fill('Like Author')
-      await page.locator('form input').nth(2).fill('http://like.com')
-      await page.getByRole('button', { name: 'create' }).click()
-
-      const blog = page.getByText('Blog to Like Like Author')
+      const blog = page.getByText('Blog to Delete Delete Author')
       await blog.getByRole('button', { name: 'view' }).click()
-      await page.getByRole('button', { name: 'like' }).click()
 
-      await expect(page.getByText('likes 1')).toBeVisible()
+      page.on('dialog', dialog => dialog.accept())
+      await page.getByRole('button', { name: 'remove' }).click()
+
+      await expect(page.getByText('Blog to Delete Delete Author')).not.toBeVisible()
     })
   })
 })
