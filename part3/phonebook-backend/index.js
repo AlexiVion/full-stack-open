e7416de4e403c16510c9fea103f1e4a62c470046
Alexi <alexivion29@gmail.com>
2026-08-23 20:21:47 -0300

    part3: exercise 3.13 phonebook database step1

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,9 @@
-﻿const express = require('express')
+﻿require('dotenv').config()
+const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
+
 const app = express()
 
 app.use(express.static('dist'))
@@ -10,61 +13,10 @@ app.use(express.json())
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  { id: "1", name: "Arto Hellas", number: "040-1234567" },
-  { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
-  { id: "3", name: "Dan Abramov", number: "12-43-234345" },
-  { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
-]
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
-
-app.get('/info', (req, res) => {
-  const date = new Date()
-  res.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${date}</p>
-  `)
-})
-
-app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  const person = persons.find(p => p.id === id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  persons = persons.filter(p => p.id !== id)
-  res.status(204).end()
-})
-
-app.post('/api/persons', (req, res) => {
-  const body = req.body
-
-  if (!body.name || !body.number) {
-    return res.status(400).json({ error: 'name or number missing' })
-  }
-
-  if (persons.some(p => p.name === body.name)) {
-    return res.status(400).json({ error: 'name must be unique' })
-  }
-
-  const person = {
-    id: String(Math.floor(Math.random() * 1000000)),
-    name: body.name,
-    number: body.number
-  }
-
-  persons = persons.concat(person)
-  res.json(person)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 const PORT = process.env.PORT || 3001
