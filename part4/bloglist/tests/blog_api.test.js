2c8198de63347f0274add61923e9208c7730390e
Alexi <alexivion29@gmail.com>
2026-08-23 20:37:38 -0300

    part4: exercise 4.8 blog list tests step1

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+﻿const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const app = require('../app')
+const api = supertest(app)
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,26 @@
+﻿const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'HTML is easy',
+    author: 'Dan Abramov',
+    url: 'http://example.com/1',
+    likes: 5
+  },
+  {
+    title: 'Browser can execute only JavaScript',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://example.com/2',
+    likes: 10
+  }
+]
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs,
+  blogsInDb
+}
