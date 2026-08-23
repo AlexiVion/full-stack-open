254543e2c78e12287e3bc760ecb7c2a712a3e243
Alexi <alexivion29@gmail.com>
2026-08-23 20:47:53 -0300

    part5: exercise 5.18 bloglist e2e step2

--- part5/bloglist-e2e/tests/blog_app.spec.js
+++ part5/bloglist-e2e/tests/blog_app.spec.js
@@ -8,4 +8,22 @@ describe('Blog app', () => {
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByText('Log in to application')).toBeVisible()
   })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.locator('input[name="Username"]').fill('root')
+      await page.locator('input[name="Password"]').fill('secret')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.locator('input[name="Username"]').fill('root')
+      await page.locator('input[name="Password"]').fill('wrongpassword')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('wrong username or password')).toBeVisible()
+    })
+  })
 })
