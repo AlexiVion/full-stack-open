2e8ae05a2272f4d6ca92bb8f6ccc4fb7a695239c
Alexi <alexivion29@gmail.com>
2026-08-23 20:18:12 -0300

    part2: exercise 2.20 data for countries step3

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,6 +1,51 @@
 ﻿import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const Weather = ({ capital }) => {
+  const [weather, setWeather] = useState(null)
+  const apiKey = import.meta.env.VITE_SOME_KEY
+
+  useEffect(() => {
+    if (!capital) return
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`)
+      .then(response => {
+        setWeather(response.data)
+      })
+      .catch(() => setWeather(null))
+  }, [capital, apiKey])
+
+  if (!weather) return null
+
+  return (
+    <div>
+      <h2>Weather in {capital}</h2>
+      <p>temperature {weather.main.temp} Celsius</p>
+      <img 
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} 
+        alt={weather.weather[0].description} 
+      />
+      <p>wind {weather.wind.speed} m/s</p>
+    </div>
+  )
+}
+
+const CountryDetail = ({ country }) => (
+  <div>
+    <h1>{country.name.common}</h1>
+    <p>capital {country.capital?.[0]}</p>
+    <p>area {country.area}</p>
+    <h3>languages:</h3>
+    <ul>
+      {Object.values(country.languages || {}).map(lang => (
+        <li key={lang}>{lang}</li>
+      ))}
+    </ul>
+    <img src={country.flags.png} alt={`flag of ${country.name.common}`} width="150" />
+    {country.capital?.[0] && <Weather capital={country.capital[0]} />}
+  </div>
+)
+
 const App = () => {
   const [query, setQuery] = useState('')
   const [countries, setCountries] = useState([])
@@ -29,20 +74,7 @@ const App = () => {
             </div>
           ))
         )}
-        {filtered.length === 1 && (
-          <div>
-            <h1>{filtered[0].name.common}</h1>
-            <p>capital {filtered[0].capital?.[0]}</p>
-            <p>area {filtered[0].area}</p>
-            <h3>languages:</h3>
-            <ul>
-              {Object.values(filtered[0].languages || {}).map(lang => (
-                <li key={lang}>{lang}</li>
-              ))}
-            </ul>
-            <img src={filtered[0].flags.png} alt={`flag of ${filtered[0].name.common}`} width="150" />
-          </div>
-        )}
+        {filtered.length === 1 && <CountryDetail country={filtered[0]} />}
       </div>
     </div>
   )
