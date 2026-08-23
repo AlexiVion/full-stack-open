4886d2590fe803b44f1ab832bbb3b0efb00ad7ec
Alexi <alexivion29@gmail.com>
2026-08-23 20:22:47 -0300

    part3: exercise 3.22 lint configuration

---
+++ part3/phonebook-backend/eslint.config.js
@@ -0,0 +1,25 @@
+﻿const js = require('@eslint/js')
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        process: 'readonly',
+        console: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly'
+      }
+    },
+    rules: {
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { 'before': true, 'after': true }],
+      'no-console': 0
+    }
+  }
+]
