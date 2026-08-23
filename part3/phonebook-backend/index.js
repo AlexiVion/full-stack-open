924728af069b7ca214fb83cf7c271938810fe927
Alexi <alexivion29@gmail.com>
2026-08-23 20:19:35 -0300

    part3: exercise 3.5 phonebook backend step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 ﻿const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   { id: "1", name: "Arto Hellas", number: "040-1234567" },
   { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
@@ -35,6 +37,19 @@ app.delete('/api/persons/:id', (req, res) => {
   const id = req.params.id
   persons = persons.filter(p => p.id !== id)
   res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  const person = {
+    id: String(Math.floor(Math.random() * 1000000)),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
 })
 
 const PORT = 3001
