676868fe5e7bd7b6f565adce26b2ef39338817a5
Alexi <alexivion29@gmail.com>
2026-08-23 20:15:21 -0300

    part2: exercise 2.5 separate modules

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,31 +1,4 @@
-﻿const Header = ({ course }) => <h2>{course}</h2>
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-)
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <b>total of {total} exercises</b>
-}
-
-const Course = ({ course }) => (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-)
+﻿import Course from './components/Course'
 
 const App = () => {
   const courses = [
