08b77b3bc35ecac515c8556f0feb3492ec5aee6f
Alexi <alexivion29@gmail.com>
2026-08-23 21:30:54 -0300

    10.4: add appbar component

---
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -0,0 +1,26 @@
+﻿import { View, StyleSheet, Text } from 'react-native';
+import Constants from 'expo-constants';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#24292e',
+    paddingBottom: 15,
+    paddingLeft: 10,
+  },
+  text: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Repositories</Text>
+    </View>
+  );
+};
+
+export default AppBar;
