8b5d41485fb5c645a63cd12990a32cd6f5384308
Alexi <alexivion29@gmail.com>
2026-08-23 21:34:55 -0300

    10.18: store access token in storage on signin and reset apollo store

--- part10/rate-repository-app/src/hooks/useSignIn.js
+++ part10/rate-repository-app/src/hooks/useSignIn.js
@@ -1,7 +1,10 @@
-﻿import { useMutation } from '@apollo/client';
+﻿import { useMutation, useApolloClient } from '@apollo/client';
 import { AUTHENTICATE } from '../graphql/mutations';
+import useAuthStorage from './useAuthStorage';
 
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
   const [mutate, result] = useMutation(AUTHENTICATE);
 
   const signIn = async ({ username, password }) => {
@@ -10,6 +13,12 @@ const useSignIn = () => {
         credentials: { username, password },
       },
     });
+
+    if (data?.authenticate?.accessToken) {
+      await authStorage.setAccessToken(data.authenticate.accessToken);
+      await apolloClient.resetStore();
+    }
+
     return data;
   };
 
