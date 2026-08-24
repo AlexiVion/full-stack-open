2fc8f383eecdf54604ce2ae772e30217c467384d
Alexi <alexivion29@gmail.com>
2026-08-23 21:30:53 -0300

    10.2: add main component

---
+++ part10/rate-repository-app/src/components/Main.jsx
@@ -0,0 +1,18 @@
+﻿import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Rate Repository Application</Text>
+    </View>
+  );
+};
+
+export default Main;
