e4de121947876367bd54af4938348f6067d17bee
Alexi <alexivion29@gmail.com>
2026-08-23 22:07:31 -0300

    11.1 - 11.4: add lightweight pokedex app and pipeline

---
+++ part11/pokedex/index.js
@@ -0,0 +1,15 @@
+﻿const http = require('http');
+const PORT = process.env.PORT || 3001;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Pokedex CI/CD App');
+});
+
+if (require.main === module) {
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = server;
---
+++ part11/pokedex/test/app.test.js
@@ -0,0 +1,6 @@
+﻿const test = require('node:test');
+const assert = require('node:assert');
+
+test('verificación básica de la aplicación', () => {
+  assert.strictEqual(1 + 1, 2);
+});
