3969292a94e745a3d29b59bbaefa3e04bc1325c8
Alexi <alexivion29@gmail.com>
2026-08-23 21:34:55 -0300

    10.19: add me query and sign out tab in appbar

--- part10/rate-repository-app/src/components/AppBar.jsx
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -1,8 +1,11 @@
-﻿import { View, StyleSheet, ScrollView } from 'react-native';
-import { Link } from 'react-router-native';
+﻿import { View, StyleSheet, ScrollView, Pressable } from 'react-native';
+import { Link, useNavigate } from 'react-router-native';
 import Constants from 'expo-constants';
+import { useQuery, useApolloClient } from '@apollo/client';
 import Text from './Text';
 import theme from '../theme';
+import { GET_ME } from '../graphql/queries';
+import useAuthStorage from '../hooks/useAuthStorage';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,20 +22,49 @@ const styles = StyleSheet.create({
   },
 });
 
-const AppBarTab = ({ title, to }) => (
-  <Link to={to} style={styles.tab}>
-    <Text fontWeight="bold" fontSize="subheading" style={{ color: theme.colors.white }}>
-      {title}
-    </Text>
-  </Link>
-);
+const AppBarTab = ({ title, to, onPress }) => {
+  if (onPress) {
+    return (
+      <Pressable onPress={onPress} style={styles.tab}>
+        <Text fontWeight="bold" fontSize="subheading" style={{ color: theme.colors.white }}>
+          {title}
+        </Text>
+      </Pressable>
+    );
+  }
+
+  return (
+    <Link to={to} style={styles.tab}>
+      <Text fontWeight="bold" fontSize="subheading" style={{ color: theme.colors.white }}>
+        {title}
+      </Text>
+    </Link>
+  );
+};
 
 const AppBar = () => {
+  const { data } = useQuery(GET_ME);
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+  const navigate = useNavigate();
+
+  const signOut = async () => {
+    await authStorage.removeAccessToken();
+    await apolloClient.resetStore();
+    navigate('/');
+  };
+
+  const me = data?.me;
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.scrollContainer}>
         <AppBarTab title="Repositories" to="/" />
-        <AppBarTab title="Sign in" to="/signin" />
+        {me ? (
+          <AppBarTab title="Sign out" onPress={signOut} />
+        ) : (
+          <AppBarTab title="Sign in" to="/signin" />
+        )}
       </ScrollView>
     </View>
   );
