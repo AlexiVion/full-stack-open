e7f169c7becade36e98cd2153557d2aa490fb754
Alexi <alexivion29@gmail.com>
2026-08-23 21:36:57 -0300

    10.24: add create review form and mutation hook

---
+++ part10/rate-repository-app/src/components/CreateReview.jsx
@@ -0,0 +1,83 @@
+﻿import { View, Pressable, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { useNavigate } from 'react-router-native';
+import FormikTextInput from './FormikTextInput';
+import Text from './Text';
+import theme from '../theme';
+import useCreateReview from '../hooks/useCreateReview';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 15,
+  },
+  button: {
+    backgroundColor: theme.colors.primary,
+    padding: 15,
+    borderRadius: 4,
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+const initialValues = {
+  ownerName: '',
+  repositoryName: '',
+  rating: '',
+  text: '',
+};
+
+const validationSchema = yup.object().shape({
+  ownerName: yup.string().required('Repository owner name is required'),
+  repositoryName: yup.string().required('Repository name is required'),
+  rating: yup
+    .number()
+    .min(0, 'Rating must be between 0 and 100')
+    .max(100, 'Rating must be between 0 and 100')
+    .required('Rating is required'),
+  text: yup.string(),
+});
+
+const CreateReview = () => {
+  const [createReview] = useCreateReview();
+  const navigate = useNavigate();
+
+  const onSubmit = async (values) => {
+    try {
+      const data = await createReview(values);
+      if (data?.createReview?.repositoryId) {
+        navigate(/repository/);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ handleSubmit }) => (
+        <View style={styles.container}>
+          <FormikTextInput name="ownerName" placeholder="Repository owner name" />
+          <FormikTextInput name="repositoryName" placeholder="Repository name" />
+          <FormikTextInput name="rating" placeholder="Rating between 0 and 100" keyboardType="numeric" />
+          <FormikTextInput name="text" placeholder="Review" multiline />
+          <Pressable onPress={handleSubmit} style={styles.button}>
+            <Text style={styles.buttonText}>Create a review</Text>
+          </Pressable>
+        </View>
+      )}
+    </Formik>
+  );
+};
+
+export default CreateReview;
